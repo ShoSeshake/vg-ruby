677b337cd3cc10b6fa0df7b929fc375e5159b99b
ShoSeshake <sho122seshake@gmail.com>
2019-11-07 19:55:47 +0900

    compelte user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-    @ingredient = Ingredient.find(params[:id])
+    @user = User.find(params[:id])
+    @commented_recipes = []
+    @user.comments.each do |c|
+      recipe = Recipe.find(c.recipe_id)
+      @commented_recipes << recipe
+    end
   end
 end
