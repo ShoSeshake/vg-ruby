6ea2747aca9d9a9f014099e5fdb4b3612b0a722c
ShoSeshake <sho122seshake@gmail.com>
2019-11-09 15:07:01 +0900

    modified issues

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -1,21 +1,23 @@
 $(function() {
-    var el = document.getElementById('instruction_box');
-    var sortable = Sortable.create(el, {
-        sort: 1,
-        group: {
-            name: 'common_lists',
-            handle: '.instruction-number__form ',
-            pull: 'clone',
-            put: true
-        },
-        onEnd: function() {
-            var positionForm = $('.instruction-number__form');
-            var i = 1;
-            positionForm.each(function() {
-                $(this).val(i);
-                i++;
-            });
-        },
-        animation: 150
-    });
+    if (window.location.href.match(/\/recipes\/\d+\/edit/) || window.location.href.match(/\/recipes\/new/)) {
+        var el = document.getElementById('instruction_box');
+        var sortable = Sortable.create(el, {
+            sort: 1,
+            group: {
+                name: 'common_lists',
+                handle: '.instruction-number__form ',
+                pull: 'clone',
+                put: true
+            },
+            onEnd: function() {
+                var positionForm = $('.instruction-number__form');
+                var i = 1;
+                positionForm.each(function() {
+                    $(this).val(i);
+                    i++;
+                });
+            },
+            animation: 150
+        });
+    }
 })
