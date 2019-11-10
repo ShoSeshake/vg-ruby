3b4d9efbb05064464b01128565b7683b48504c44
ShoSeshake <sho122seshake@gmail.com>
2019-11-10 09:51:14 +0900

    modal and btn complete

---
+++ app/assets/javascripts/restaurant_show.js
@@ -0,0 +1,23 @@
+$(function() {
+    var vfModal = $('#vf-level-modal');
+    var deleteBtn = $('#delete-btn');
+    var vfBtn = $('.restaurant-show__box__level');
+    vfBtn.on('click', function() {
+        vfModal.fadeIn(500);
+    })
+    vfModal.on('click', function() {
+        vfModal.fadeOut(500);
+    })
+    var detailBtn = $('#detail-btn');
+    var commentBtn = $('#comment-btn');
+    var detailBox = $('.restaurant-show__detail');
+    var commentBox = $('.restaurant-show__comment');
+    detailBtn.on('click', function() {
+        detailBox.fadeIn(500);
+        commentBox.hide();
+    })
+    commentBtn.on('click', function() {
+        commentBox.fadeIn(500);
+        detailBox.hide();
+    })
+})
