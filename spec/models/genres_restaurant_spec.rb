a0742ffdbca81b860f9eeba7a5ec8cd60e310546
ShoSeshake <sho122seshake@gmail.com>
2019-11-13 00:02:00 +0900

    test code

--- spec/models/genres_restaurant_spec.rb
+++ spec/models/genres_restaurant_spec.rb
@@ -1,4 +1,21 @@
-# class GenresRestaurant < ApplicationRecord
-#   belongs_to :restaurant
-#   belongs_to :genre
-# end
+require 'rails_helper'
+describe GenresRestaurant do
+  describe '#create' do
+    it "is valid with a genre, restaurant" do
+      genres_restaurant = build(:genres_restaurant)
+      expect(genres_restaurant).to be_valid
+    end
+
+    it "is invalid without a genre" do
+      genres_restaurant = build(:genres_restaurant, genre: nil)
+      genres_restaurant.valid?
+      expect(genres_restaurant.errors[:genre]).to include("must exist")
+    end
+
+    it "is invalid without a restaurant" do
+      genres_restaurant = build(:genres_restaurant, restaurant: nil)
+      genres_restaurant.valid?
+      expect(genres_restaurant.errors[:restaurant]).to include("must exist")
+    end
+  end
+end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,6 +1,21 @@
-# class Image < ApplicationRecord
-#   belongs_to :recipe
-#   mount_uploader :url, ImageUploader
+require 'rails_helper'
+describe Image do
+  describe '#create' do
+    it "is valid with a url" do
+      image = build(:image)
+      expect(image).to be_valid
+    end
 
-#   validates :url, presence: true
-# end
+    it "is valid with a content, without an url" do
+      image = build(:image, url: nil)
+      image.valid?
+      expect(image.errors[:url]).to include("can't be blank")
+    end
+
+    it "is invalid without a recipe" do
+      image = build(:image, recipe: nil)
+      image.valid?
+      expect(image.errors[:recipe]).to include("must exist")
+    end
+  end
+end
