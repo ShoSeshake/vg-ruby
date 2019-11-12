31f8aaad198b0d757035b2479d1822a35365cc8e
ShoSeshake <sho122seshake@gmail.com>
2019-11-13 00:04:36 +0900

    test code

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,3 +3,36 @@
 #   belongs_to :user
 #   validates :text, :review, presence: true
 # end
+require 'rails_helper'
+describe Comment do
+  describe '#create' do
+    it "is valid with a text, review" do
+      comment = build(:comment)
+      expect(comment).to be_valid
+    end
+
+    it "is valid with a content, without a text" do
+      comment = build(:comment, text: nil)
+      comment.valid?
+      expect(comment.errors[:text]).to include("can't be blank")
+    end
+
+    it "is valid with a content, without a review" do
+      comment = build(:comment, review: nil)
+      comment.valid?
+      expect(comment.errors[:review]).to include("can't be blank")
+    end
+
+    it "is invalid without a recipe" do
+      comment = build(:comment, recipe: nil)
+      comment.valid?
+      expect(comment.errors[:recipe]).to include("must exist")
+    end
+
+    it "is invalid without a user" do
+      comment = build(:comment, user: nil)
+      comment.valid?
+      expect(comment.errors[:user]).to include("must exist")
+    end
+  end
+end
