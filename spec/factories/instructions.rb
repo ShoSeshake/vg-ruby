5b8b9e8c448e6c1ca3d6dbae99df4fc72a29dfb3
ShoSeshake <sho122seshake@gmail.com>
2019-11-12 23:52:50 +0900

    test code

--- spec/factories/instructions.rb
+++ spec/factories/instructions.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :instruction, class: Instruction do
     text              {Faker::Quote.famous_last_words}
     position             {1}
+    recipe
   end
 end
--- spec/models/instruction_spec.rb
+++ spec/models/instruction_spec.rb
@@ -1,4 +1,27 @@
-# class Instruction < ApplicationRecord
-#   belongs_to :recipe
-#   validates :text, :position, presence: true
-# end
+require 'rails_helper'
+describe Instruction do
+  describe '#create' do
+    it "is valid with a text, position" do
+      instruction = build(:instruction)
+      expect(instruction).to be_valid
+    end
+
+    it "is valid with a text, without an position" do
+      instruction = build(:instruction, position: nil)
+      instruction.valid?
+      expect(instruction.errors[:position]).to include("can't be blank")
+    end
+
+    it "is valid with an position, without a text" do
+      instruction = build(:instruction, text: nil)
+      instruction.valid?
+      expect(instruction.errors[:text]).to include("can't be blank")
+    end
+
+    it "is invalid without a recipe" do
+      instruction = build(:instruction, recipe: nil)
+      instruction.valid?
+      expect(instruction.errors[:recipe]).to include("must exist")
+    end
+  end
+end
