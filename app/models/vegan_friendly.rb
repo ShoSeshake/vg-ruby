e83142cb2cc234d3fb180a099d2ec5c400406ac1
ShoSeshake <sho122seshake@gmail.com>
2019-11-18 10:15:45 +0900

    modified

--- app/models/vegan_friendly.rb
+++ app/models/vegan_friendly.rb
@@ -1,6 +1,6 @@
-class VeganFriendly < ActiveHash::Base
-  self.data = [
-      {id: 1, name: 'Vegan'}, {id: 2, name: 'Vegan-Friendly'}, {id: 3, name: 'Vegetarian'},
-      {id: 4, name: ' Vegetarian (But Not Vegan-Friendly)'},{id: 5, name:'Vegetarian-Friendly'}
-  ]
-  end
+  class VeganFriendly < ActiveHash::Base
+    self.data = [
+        {id: 1, name:'Vegetarian-Friendly'},{id: 2, name: ' Vegetarian (But Not Vegan-Friendly)'},{id: 3, name: 'Vegetarian'},
+        {id: 4, name: 'Vegan-Friendly'},{id: 5, name: 'Vegan'}
+    ]
+    end
